bda4f5b110995ae5a49008ea6cc0ec01323a43c5
csterling <coreytsterling@gmail.com>
2021-08-06 11:13:52 +1200

    Refactored string repetition.

--- src/māia/util/string.kt
+++ src/māia/util/string.kt
@@ -5,42 +5,80 @@ package māia.util
  */
 
 /**
- * Extension functions which defines string-multiplication
- * in the Python way (the same string repeated the given
- * number of times).
+ * Returns the given string repeated the given number
+ * of times.
  *
- * @receiver    The string to repeat.
- * @param x     The number of times to repeat the string.
- * @return      The repeated string.
+ * @param string
+ *          The string to repeat.
+ * @param times
+ *          The number of times to repeat the string.
+ * @return
+ *          The repetitive string.
  */
-operator fun String.times(x : Int) : String {
-    if (x < 0) throw IllegalArgumentException("Can't multiply a string a negative number of times ($x)")
-    return buildString {
-        for (i in 0 until x) append(this@times)
+inline fun repeatString(string: String, times: Int): String {
+    // If repeating zero times, the result is always the empty string
+    if (times == 0) return ""
+
+    require(times > 0) {
+        "Can't repeat a string a negative number of times (${times})"
+    }
+
+    // The empty string repeated any number of times is the empty string
+    if (string == "") return ""
+
+    require(times <= Int.MAX_VALUE / string.length) {
+        "Repeating '${string}' $times times would overflow the resulting string"
+    }
+
+    return buildString(string.length * times) {
+        inlineForLoop({ 0 }, { it < times }, { it.inc() }) {
+            append(string)
+        }
     }
 }
 
 /**
- * Extension functions which defines string-multiplication
+ * Extension function which defines string-multiplication
  * in the Python way (the same string repeated the given
  * number of times).
  *
- * @receiver    The number of times to repeat the string.
- * @param x     The string to repeat.
- * @return      The repeated string.
+ * @receiver
+ *          The string to repeat.
+ * @param times
+ *          The number of times to repeat the string.
+ * @return
+ *          The repetitive string.
  */
-operator fun Int.times(x : String) : String = x * this
+operator fun String.times(times : Int) : String = repeatString(this, times)
+
+/**
+ * Extension function which defines string-multiplication
+ * in the Python way (the same string repeated the given
+ * number of times).
+ *
+ * @receiver
+ *          The number of times to repeat the string.
+ * @param string
+ *          The string to repeat.
+ * @return
+ *          The repetitive string.
+ */
+operator fun Int.times(string : String) : String = repeatString(string, this)
 
 /**
  * Indents a string.
  *
- * @receiver            The string to indent.
- * @param amount        The number of times to indent.
- * @param indentation   The string to use to indent with.
- * @return              The indented string.
+ * @receiver
+ *          The string to indent.
+ * @param amount
+ *          The number of times to indent.
+ * @param indentation
+ *          The string to use to indent with.
+ * @return
+ *          The indented string.
  */
 fun String.indent(amount : Int = 1, indentation : String = " ") : String {
-    val finalIndentation = amount * indentation
+    val finalIndentation = repeatString(indentation, amount)
     return split("\n").joinToString("\n") {
         finalIndentation + it
     }
