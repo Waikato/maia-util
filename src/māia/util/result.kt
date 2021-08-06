ec8b1b0378cb5caacb577a540cbf7106e9fc1a99
csterling <coreytsterling@gmail.com>
2021-08-06 15:57:42 +1200

    Added comments.

--- src/māia/util/result.kt
+++ src/māia/util/result.kt
@@ -1,11 +1,31 @@
 package māia.util
 
+/**
+ * Represents the (possibly-erroneous) result of some operation.
+ *
+ * @param T
+ *          The type of the successful result.
+ * @param E
+ *          The type of the erroneous result.
+ */
 sealed class Result<out T, out E>
 
+/**
+ * Represents the succesful result of an operation.
+ *
+ * @param T
+ *          The type of the successful result.
+ */
 class Success<out T>(
     val value: T
 ) : Result<T, Nothing>()
 
+/**
+ * Represents the erroneous result of an operation.
+ *
+ * @param E
+ *          The type of the erroneous result.
+ */
 class Failure<out E>(
     val error: E
 ) : Result<Nothing, E>()
